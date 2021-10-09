6e1d23e59587f6a0e1e257c346303e4e2171fd13
Deniss Orlov <aristarxus@Denisss-MacBook-Pro.local>
2021-10-09 14:43:06 +0300

    update fercher, add timeout

--- front/src/App.js
+++ front/src/App.js
@@ -2,14 +2,28 @@
 import {useState, useEffect} from 'react'
 import './App.css';
 
-function App() {
-  const [state, setState] = useState([])
-    useEffect(() => {
-        fetch("http://localhost:5050/api/v1/readings/")
-        .then(res => res.json())
+const useFetch = url => {
+  const [state, setState] = useState()
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetch(url)
+        .then(data => data.json())
         .then(data => setState(data))
-        .catch(err => console.log(err))
-    })
+        .catch(function(error) {
+          console.log(error)
+        })
+    }, 5000)
+
+    return () => clearInterval(interval);
+  }, [url])
+
+  return state
+}
+
+function App() {
+  const state = useFetch("http://localhost:5050/api/v1/readings/")
+  
 
   console.log(state)
   return (
